b504954ed422c0c8235a71c11307e4e627d37745
mrutch <55161223+mrutch@users.noreply.github.com>
2023-10-21 13:09:14 -0500

    updated base client file

--- clueless_application/Inventory/character.py
+++ clueless_application/Inventory/character.py
@@ -1,8 +1,8 @@
 class Character:
-    def __init__(self,name, location, turn, inventory, history):
+    def __init__(self,name, location, turn, inventory, history, madeAccusation):
         self.name = name
         self.location = location
-        self.accusation_avail = False
+        self.madeAccusation = False
         self.turn = False
         self.inventory = []
         self.history = []
--- clueless_application/client.py
+++ clueless_application/client.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('../')
 import socket
 from messaging.message import Message
+import validateDisprove
 
 class CluelessClient:
     def __init__(self, host, port):
@@ -23,15 +24,34 @@ class CluelessClient:
 
     def makeMove(self):
         print(f"Player {self.host} decided to make a move")
+        #TODO Figure out how we get move value
+        # TODO figure out how to get character value
+        validateMove(self, self.character, move)
+
 
     def makeSuggesstion(self):
         print(f"Player {self.host} decided to make a sugesstion")
+        weapon = input('Please enter the weapon you think was used: ')
+        suggesstedCharacter = input('Please enter who you think committed the crime: ')
+        #TODO figure out character and location
+        validateSuggestion(self, character, character.location, weapon, suggesstedCharacter)
 
     def makeAccusation(self):
-        print(f"Player {self.host} decided to make an accusation")
-    
+        print(f"Player {self.host} decided to make an accusation.")
+        room = input('Please enter the room you think the crime occured: ')
+        weapon = input('Please enter the weapon you think was used: ')
+        suggesstedCharacter = input('Please enter who you think committed the crime: ')
+        validateAccusation(self, self.host, room, weapon, suggesstedCharacter)
+
     def disproveSuggestion(self):
-        print(f"Player {self.host} please enter the item to disprove the other player")
+        item = input(f"Player {self.host} please enter the item to disprove the other player. If you can not disprove enter no")
+        if(item == 'no'){
+            validateDisprove(self, false, item)
+        }
+        else{
+            validateDisprove(self, true, item)
+        }
+    
 
     def processMessage(self, message):
         print(f"Processing Message: {message}")
