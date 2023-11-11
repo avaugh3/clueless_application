cdf08faa4f64699c21e4308165544ca0dd864622
mrutch <55161223+mrutch@users.noreply.github.com>
2023-11-11 12:55:05 -0600

    updated more inventory classes

--- clueless_application/Inventory/hallway.py
+++ clueless_application/Inventory/hallway.py
@@ -1,5 +1,16 @@
-class Room:
+class Hallway:
    def __init__(self, location, occupied, occupiedBy):
         self.location = location
         self.occupied = occupied
         self.occupiedBy = occupiedBy
+   
+   def moveIntoHallway(self, Character):
+      if self.occupied
+         print('Sorry! This hallway is already occupied')
+      else
+         self.occupied = True
+         self.occupiedBy = Character
+
+   def moveOutOfHallway():
+      self.occupied = False
+      self.occupiedBy = ''
