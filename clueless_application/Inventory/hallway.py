9b40d6481b69866f66d623029066ad59c451847c
Aaron Reed <ajreeddev@gmail.com>
2023-12-09 19:12:05 -0500

    Add more getters for Room and Hallway classes (actual)

--- clueless_application/Inventory/hallway.py
+++ clueless_application/Inventory/hallway.py
@@ -1,9 +1,21 @@
+from typing import Any
+
+
 class Hallway:
    def __init__(self, location, occupied, occupiedBy):
         self.location = location
         self.occupied = occupied
         self.occupiedBy = occupiedBy
    
+   def getLocation(self):
+      return self.location
+   
+   def isOccupied(self):
+      return self.occupied
+   
+   def isOccupiedBy(self):
+      return self.occupiedBy
+
    def moveIntoHallway(self, Character):
       if self.occupied:
          print('Sorry! This hallway is already occupied')
@@ -14,3 +26,5 @@ class Hallway:
    def moveOutOfHallway(self):
       self.occupied = False
       self.occupiedBy = ''
+
+  
--- clueless_application/Inventory/room.py
+++ clueless_application/Inventory/room.py
@@ -5,6 +5,18 @@ class Room:
         self.occupied = occupied
         self.occupiedBy = occupiedBy
 
+   def getName(self):
+      return self.roomName
+   
+   def getLocation(self):
+      return self.location
+   
+   def isOccupied(self):
+      return self.occupied
+   
+   def isOccupiedBy(self):
+      return self.occupiedBy
+
    def joinRoom(self, character):
       self.occupied = True
       self.occupiedBy.add(character)
@@ -15,4 +27,6 @@ class Room:
       self.occupiedBy.remove(character)
       if (self.occupiedBy.length == 0):
          self.occupied = False
-      
+
+   def getRoomLocation(self, location):
+      return location     
